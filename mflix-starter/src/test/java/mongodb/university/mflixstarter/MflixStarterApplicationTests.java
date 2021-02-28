9d3de1c7ef4f4a6e5052e65960d8b38c717767ca
khanh chau <kchau158@gmail.com>
2021-02-28 07:12:05 -0500

    Initial commit

---
+++ mflix-starter/src/test/java/mongodb/university/mflixstarter/MflixStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package mongodb.university.mflixstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MflixStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
